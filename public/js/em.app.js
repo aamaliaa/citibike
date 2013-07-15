c00f143911c7e3b9a74b7824756ac40232f2a114
Amalia Viti <amalia.viti@gmail.com>
2013-07-15 17:20:55 -0400

    create views, make initial google maps load

--- public/js/em.app.js
+++ public/js/em.app.js
@@ -1,3 +1,17 @@
+var config = {
+	nycBounds: new google.maps.LatLngBounds(new google.maps.LatLng(40.666577080451354, -74.036865234375), new google.maps.LatLng(40.879775645515764, -73.85078430175781)),
+	map: {
+		center: new google.maps.LatLng(40.73492695, -73.99200509),
+		zoom: 13,
+		maxZoom: 18,
+		minZoom: 13,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeControl: false,
+		streetViewControl: false,
+		backgroundColor: '#ffffff'
+	}
+};
+
 var App = Em.Application.create({
 	autoinit: false,
 	LOG_TRANSITIONS: true
@@ -8,6 +22,53 @@ App.ApplicationController = Em.Controller.extend();
 App.ApplicationView = Em.View.extend({
 	templateName: 'appView',
 	elementId: 'app'
+});
+
+App.MapController = Em.Controller.extend();
+
+App.MapView = Em.View.extend({
+	templateName: 'mapView',
+	map: null,
+	bikeLayer: null,
+	geocoder: null,
+	directionsRenderer: null,
+	directionsService: null,
+	overlay: null,
+	didInsertElement: function(){
+		// set height and width
+		$('#map-canvas').css("height", $(window).height()+"px");
+		$('#map-canvas').css("width", $(window).width()+"px");
+
+		google.maps.visualRefresh = true; // new gmaps style
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'), config.map);
+		
+		this.set('map', map);
+
+		var maps_vars = {
+			bikeLayer: new google.maps.BicyclingLayer(),
+			geocoder: new google.maps.Geocoder(),
+			directionsRenderer: new google.maps.DirectionsRenderer(),
+			directionsService: new google.maps.DirectionsService(),
+			overlay: new google.maps.OverlayView()
+		};
+
+		for(var name in maps_vars){
+			this.set(name, maps_vars[name]);
+		}
+
+		this.bikeLayer.setMap(this.map);
+		this.directionsRenderer.setMap(this.map);
+
+		google.maps.event.addListenerOnce(this.map, 'tilesloaded', App.MapController.addMarkers);
+	},
+	handleResize: function(){
+		google.maps.event.trigger(this.map, 'resize');
+	}
+});
+
+App.SidebarView = Em.View.extend({
+	templateName: 'sidebarView'
 });
 
 App.initialize();
--- public/js/templates.js
+++ public/js/templates.js
@@ -1,9 +1,38 @@
 Ember.TEMPLATES["appView"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [3,'>= 1.0.0-rc.4'];
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  var buffer = '', hashTypes, hashContexts, escapeExpression=this.escapeExpression;
+
+
+  data.buffer.push("<div id=\"map\">");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers.view.call(depth0, "App.MapView", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</div>\r\n<div id=\"key\">");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers.view.call(depth0, "App.SidebarView", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</div>");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["mapView"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [3,'>= 1.0.0-rc.4'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   
 
 
-  data.buffer.push("<div id=\"map\">\r\n	<div id=\"map-canvas\"></div>\r\n</div>\r\n	\r\n<div id=\"key\">\r\n	<div class=\"content\">\r\n		<p>Where do you want to go?</p>\r\n		<p>from</p>\r\n		<div class=\"input\"><span class=\"label\">A</span><input id=\"origin\" type=\"text\" placeholder=\"address or location\"></div>\r\n		<p>to</p>\r\n		<div class=\"input\"><span class=\"label\">B</span><input id=\"destination\" type=\"text\" placeholder=\"address or location\"></div>\r\n		<input type=\"button\" class=\"btn-primary\" value=\"plot\" onclick=\"setDestination()\">\r\n	</div>\r\n</div>");
+  data.buffer.push("<div id=\"map-canvas\"></div>");
+  
+});
+
+Ember.TEMPLATES["sidebarView"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [3,'>= 1.0.0-rc.4'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("<div class=\"content\">\n	<p>Where do you want to go?</p>\n	<p>from</p>\n	<div class=\"input\"><span class=\"label\">A</span><input id=\"origin\" type=\"text\" placeholder=\"address or location\"></div>\n	<p>to</p>\n	<div class=\"input\"><span class=\"label\">B</span><input id=\"destination\" type=\"text\" placeholder=\"address or location\"></div>\n	<input type=\"button\" class=\"btn-primary\" value=\"plot\" onclick=\"setDestination()\">\n</div>");
   
 });
